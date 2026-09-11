31b41586f9d1ebe0409b1c2f33f1395ebe1eaa37
Aldmiro Quizenga <primeenterprises023@gmail.com>
2026-09-11 23:58:18 +0100

    Aprovar carros automaticamente

--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -43,7 +43,7 @@ router.post("/", authMiddleware, async (req, res) => {
           location,
           description,
           image_url,
-          status: "pending"
+          status: "approved"
         }
       ])
       .select()
