92de21e0692f96b07253a3d319dc1ef3d82cf479
primeenterprises023-ai <primeenterprises023@gmail.com>
2026-09-18 12:03:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors({
   origin: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
